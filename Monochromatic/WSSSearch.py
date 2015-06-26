cb41f7ef5c3a65582a7b2466dd5fdd673a5b1c79
simsim314 <simsim314@gmail.com>
2015-06-26 15:59:28 +0300

    Bug fix - First SL in each recipe was not reported

--- Monochromatic/WSSSearch.py
+++ Monochromatic/WSSSearch.py
@@ -1,12 +1,58 @@
 import golly as g 
 from copy import copy
 
-destinationPath = "C:\\Users\\SimSim314\\Glue\\Monochromaticv2\\"
+destinationPath = "C:\\Users\\SimSim314\\Glue\\AllWSS\\"
 gld = g.parse("3o$o$bo!")
 blck = g.parse("2o$2o!")
 existingDic = {}
 existingKeys = []
 
+def FindRightMost(cells):
+	maxX = -10000
+	
+	for i in xrange(1, len(cells), 2):
+		if cells[i - 1] > maxX:
+			maxX = cells[i - 1]
+			
+	return maxX
+	
+def HasEdgeShooter(minx):
+	rect = g.getrect()
+	
+	y = rect[1] + rect[3]
+	
+	if y < 70: 
+		return False
+		
+	g.run(4)
+	
+	rect = g.getrect()
+	
+	if y + 2 == rect[1] + rect[3]:
+	
+		maxX = -10000
+		maxL = -10000
+		
+		for i in xrange(0, 4):
+		
+			g.run(1)
+			rect = g.getrect()
+			curCells = g.getcells([rect[0], rect[1] + rect[3] - 10, rect[2], 11])
+			curx = FindRightMost(curCells)
+			curL = len(curCells)
+			
+			if curx > maxX:
+				maxX = curx
+				
+			if curL > maxL:
+				maxL = curL
+		
+		if minx - 2 > maxL:
+			return 100 * maxL
+			
+		return maxL
+	
+	return False
 def FindActive():
 
    rect = g.getrect()
@@ -59,6 +105,10 @@ def CountSL():
 	return result
   
 def PlaceRecipeList(recipes):
+	
+	if len(recipes) == 0:
+		return 
+	
 	g.new("")
 	dx = 0
 	
@@ -86,25 +136,31 @@ def PlaceRecipeList(recipes):
 			f = open(destinationPath + str(len(recipes)) + curF + ".txt",'w')
 			
 		f.write("{0},{1}:".format(x1, y1) + str(recipe).strip("]").strip("[") + "\n")
-			
+
 def EvolveRecipe(recipe):
 	g.new("")
 	g.setstep(3)
 	
 	g.putcells(blck)
 	
+	minx = g.getrect()[0]
+	
 	for r in recipe:
 		g.putcells(gld, 40, 40 + r)
 		g.step()
 		g.step()
-
+		
+		if minx > g.getrect()[0]:
+			minx > g.getrect()[0]
+	
+	return minx
+	
 result = []
 	
-def FindAllHs(cells):
+def FindAllHs(cells, minx):
 	g.new("")
 	g.putcells(cells)
 	rect = g.getrect()
-	
 	
 	min = 10000
 	max = -10000
@@ -141,6 +197,11 @@ def FindAllHs(cells):
 		if g.getrect()[0] < -120:
 			continue 
 		
+		edgeType = HasEdgeShooter(minx)
+		
+		if edgeType != False:
+			answer[0].append([i, 0, 0, [edgeType]])
+			
 		rect = g.getrect()
 		
 		if rect[2] > 25 or rect[3] > 25:
@@ -152,23 +213,6 @@ def FindAllHs(cells):
 		if s == str(g.getcells(g.getrect())):
 			
 			key = str(rect) + ":" + str(g.getpop())
-			
-			SLs = []
-			
-			if rect[2] < 8 and rect[3] < 8:
-				SLs = CountSL()
-			
-			if len(SLs) == 1: 
-				SLs[0].sort(key=lambda r: 100000 * r[1] + r[0])
-				
-				x1 = SLs[0][0][0]
-				y1 = SLs[0][0][1]
-				
-				for o in xrange(0, len(SLs[0])):
-					SLs[0][o][0] -= x1
-					SLs[0][o][1] -= y1
-
-				answer[0].append([i, x1, y1, SLs[0]])
 			
 			if not (key in existingKeys):
 				existingDic[key] = []
@@ -196,10 +240,10 @@ def EvolveRecipes(recipes):
 		if cnt % 100 == 0:
 			g.show(str(cnt) + "/" + str(len(recipes)))
 		
-		EvolveRecipe(recipe)
+		minx = EvolveRecipe(recipe)
 		cells = g.getcells(g.getrect())
 		
-		answer = FindAllHs(cells)
+		answer = FindAllHs(cells, minx)
 		
 		for h in answer[0]:
 			i = h[0]
